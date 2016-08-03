0b2b49cff1b98227bb0dd8836209e82986480772
Nick Heiner <NickHeiner@users.noreply.github.com>
2016-08-02 22:37:38 -0400

    Fix tests (#7)
    Clean up dev dependencies and make the tests pass.

--- test/spawn.js
+++ test/spawn.js
@@ -6,22 +6,22 @@ var spawn = require('..')
 var uglifyjs = require.resolve('uglify-js/bin/uglifyjs')
 
 describe('co-spawn', function () {
-  it('should work with strings', co(function* () {
+  it('should work with strings', () => co(function* () {
     var out = yield spawn(uglifyjs, ['-']).end('var a = 1')
-    out.should.equal('var a=1;')
+    out.should.equal('var a=1;\n')
   }))
 
-  it('should work with streams', co(function* () {
+  it('should work with streams', () => co(function* () {
     var stream = new readable()
     stream._read = function () {
       this.push('var a = 1')
       this.push(null)
     }
     var out = yield spawn(uglifyjs, ['-']).pump(stream)
-    out.should.equal('var a=1;')
+    out.should.equal('var a=1;\n')
   }))
 
-  it('should work with errors', co(function* () {
+  it('should work with errors', () => co(function* () {
     try {
       yield spawn('laksjdflkajsdf')
       throw new Error('wtf')
